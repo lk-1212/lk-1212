0a64bd5ef465a7a6e6ac147e2f89f73a41ad0c2b
Loreto Kato <93004284+lk-1212@users.noreply.github.com>
2025-01-16 20:17:27 +0000

    Update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import AboutMe from "@/components/about_me";
 import Contacts from "@/components/contacts";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div>
-      <AboutMe />
-      <Contacts />
-    </div>
-  );
+const Home = (): React.ReactElement => {
+  return(
+    <div className="flex flex-col items-center justify-center h-screen">
+    <AboutMe />
+    <Contacts />
+  </div>
+  )
 }
+
+export default Home
