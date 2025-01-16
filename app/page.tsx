b309341d60d9b209cc44744037566ac5b2909f65
Loreto Kato <93004284+lk-1212@users.noreply.github.com>
2025-01-16 20:43:18 +0000

    Update responsive breakpoints

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Home = (): React.ReactElement => {
   return(
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col  text-center items-center justify-center h-screen">
     <AboutMe />
     <Contacts />
   </div>
--- components/about_me/index.tsx
+++ components/about_me/index.tsx
@@ -4,14 +4,14 @@ import { ABOUT_ME, NAME } from './constants'
 const AboutMe = ():React.ReactElement => {
     const renderName = (): React.ReactElement => {
         return(
-            <div className='text-[100px]'>
+            <div className='text-5xl sm:text-8xl'>
                 <p>{NAME}</p>
             </div>
         )
     }
 
     const renderaAbout = (): React.ReactElement => {
-        return <div className='text-3xl' dangerouslySetInnerHTML={{__html: ABOUT_ME}} />
+        return <div className='text-xl mt-2 sm:mt-4 sm:text-4xl' dangerouslySetInnerHTML={{__html: ABOUT_ME}} />
     }
     return(
         <div className='flex flex-col items-center'>
